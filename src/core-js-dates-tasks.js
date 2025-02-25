9ed1e512109cac7cf028265328f2757ef17cd594
Milena Belianova <milena.belianova@gmail.com>
2025-02-25 14:49:16 +0400

    feat: implement first 4 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return Date.parse(date);
 }
 
 /**
@@ -31,8 +31,12 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours().toString().padStart(2, 0);
+  const min = date.getMinutes().toString().padStart(2, 0);
+  const sec = date.getSeconds().toString().padStart(2, 0);
+
+  return `${hours}:${min}:${sec}`;
 }
 
 /**
@@ -46,8 +50,19 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const day = new Date(date).getUTCDay();
+
+  return weekDays[day];
 }
 
 /**
@@ -61,8 +76,23 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const givenDate = new Date(date);
+  const day = givenDate.getUTCDay();
+
+  let daysToAdd = 0;
+
+  if (day < 5) {
+    daysToAdd = 5 - day;
+  } else if (day === 5) {
+    daysToAdd = 7;
+  } else {
+    daysToAdd = 6;
+  }
+
+  givenDate.setUTCDate(givenDate.getUTCDate() + daysToAdd);
+
+  return givenDate;
 }
 
 /**
